c4feb807cbf91aa660b8fb6149c3a492e4fbf803
Strong <2088201923@qq.com>
2021-05-04 22:08:42 +0800

    更新网站信息

--- Src/Network/HttpClient.cpp
+++ Src/Network/HttpClient.cpp
@@ -1,7 +1,7 @@
 ﻿/**********************************************************
 Author: Qt君
 微信公众号: Qt君(文章首发)
-Website: qtbig.com(后续更新)
+Website: qthub.com(后续更新)
 Email:  2088201923@qq.com
 QQ交流群: 732271126
 LISCENSE: MIT
