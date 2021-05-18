701f476c727a16bdb2ac1551f59f672d742b9fc3
Strong <2088201923@qq.com>
2021-05-18 21:46:43 +0800

    添加使用例子

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -76,13 +76,78 @@ public:
         // [5]
 
         // [6] post文件
-        client.post("http://localhost:1234/post")
-              .bodyWithFile("text_file", "helloworld.txt")
-              .bodyWithFile("image_file", "qt.jpg")
-              .onSuccess(this, SLOT(onSuccess(QString)))
-              .onFailed(this, SLOT(onFailed(QString)))
-              .exec();
+    client.post("http://httpbin.org/post")
+          .bodyWithFile("text_file", "helloworld.txt")
+          .bodyWithFile("image_file", "qt.jpg")
+          .onUploadProgress([](qint64 bytesSent, qint64 bytesTotal) {
+              qDebug() << "lambda bytes sent: " << bytesSent
+                       << "bytes total: " << bytesTotal;
+           })
+          .onSuccess([](QString result) { qDebug()<<"result: " << result.left(100); })
+          .onFailed([](QString error) { qDebug()<<"error: " << error; })
+          .exec();
         // [6]
+
+        // [7] readyRead测试
+        client.get("https://hub.fastgit.org/aeagean/QtNetworkService/archive/refs/heads/master.zip")
+              .onReadyRead([](QNetworkReply *reply) { qDebug()<< "readyRead: "<<reply->readAll().size(); })
+              .onSuccess([](QString result) { qDebug()<<"result: success"<<result; })
+              .onFailed([](QString error) { qDebug()<<"error: "<<error; })
+              .exec();
+        // [7]
+
+        // [8] 下载文件
+        // client.get("https://down.sandai.net/thunder11/XunLeiWebSetup11.1.12.1692gw.exe")
+        client.get("https://hub.fastgit.org/aeagean/QtNetworkService/archive/refs/heads/master.zip")
+              .onDownloadProgress([](qint64 bytesReceived, qint64 bytesTotal) {
+                  qDebug() << "lambda bytes received: " << bytesReceived
+                           << "bytes total: " << bytesTotal;
+               })
+              .download()
+              .onDownloadSuccess([](QString fileName) { qDebug()<<"download success: "<<fileName; })
+              .onDownloadFailed([](QString fileName) { qDebug()<<"download failed: "<<fileName; })
+              .onFailed([](QString error) { qDebug()<<"error:"<<error; })
+              .exec();
+        // [8]
+
+        // [9] 上传文件
+        QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+        QString contentType = QString("multipart/form-data;boundary=%1").arg(multiPart->boundary().data());
+
+        QFile *file = new QFile("demo.txt");
+        file->open(QIODevice::ReadOnly);
+        file->setParent(multiPart);
+
+        QString dispositionHeader = QString("form-data; name=\"%1\";filename=\"%2\"")
+                .arg("text_file")
+                .arg(file->fileName());
+
+        QHttpPart part;
+        part.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("text/plain"));
+        part.setHeader(QNetworkRequest::ContentDispositionHeader, dispositionHeader);
+        part.setBodyDevice(file);
+
+        multiPart->append(part);
+
+        client.post("http://httpbin.org/post")
+              .header("content-type", contentType)
+              .body(multiPart)
+              .onSuccess([](QString result){})
+              .onFailed([](QString error){})
+              .exec();
+        // [9]
+
+        // [10] 携带特定的用户数据到响应回调函数
+        client.get("http://httpbin.org/get")
+              .userAttribute("Hello world!")
+              .onSuccess([](QNetworkReply *reply) {
+                    QVariant value = reply->request().attribute(QNetworkRequest::User);
+                    qDebug()<< value.toString();
+               })
+              .onFailed([](QString error){ qDebug()<<error; })
+              .exec();
+
+        // [10]
     }
 
 
@@ -130,19 +195,13 @@ int main(int argc, char *argv[])
 #if 0
     Object object;
 #else
-
     HttpClient client;
-//    client.post("http://localhost:1234/post")
     client.post("http://httpbin.org/post")
-          .bodyWithFile("text_file", "helloworld.txt")
-          .bodyWithFile("image_file", "qt.jpg")
-          .onUploadProgress([](qint64 bytesSent, qint64 bytesTotal) {
-              qDebug() << "lambda bytes sent: " << bytesSent
-                       << "bytes total: " << bytesTotal;
-           })
-          .onSuccess([](QString result) { qDebug()<<"result: " << result.left(100); })
-          .onFailed([](QString error) { qDebug()<<"error: " << error; })
+          .body("hello world")
+          .onSuccess([](QString result){qDebug()<<result;})
+          .onFailed([](QString error){qDebug()<<error;})
           .exec();
+
 #endif
 
     return a.exec();
